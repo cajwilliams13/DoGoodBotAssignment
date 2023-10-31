354ad68c52376f32c13b6ec4d69a030adea83b91
cajwilliams13 <cajwilliams13@gmail.com>
2023-11-01 08:20:15 +1100

    Gripper implemented

--- main.py
+++ main.py
@@ -1,14 +1,44 @@
 import swift
 import roboticstoolbox as rtb
 from robotcore.GantryBot.GantryBot import GantryBot
-from robotcore.UR3Lin.UR3Lin import UR3Lin
+from robotcore.Gripper.Gripper import Gripper
+from spatialmath import SE3
+
+
+def CreateEnvironment(env, transform=None):
+    env.launch(realtime = True)
+    #Add Everything to environment
+    
+    #Robots
+
+    GanBot = GantryBot()
+    #UR5Bot = rtb.models.DH.UR5()
+    GripperBot = Gripper()
+
+    #Apply Transforms
+    GanBot.base = GanBot.base @ transform
+    #UR5Bot.base = UR5Bot.base @ transform
+    GripperBot.base = GripperBot.base @ transform
+
+    #Add to Environment
+    GanBot.add_to_env(env)
+    #UR5Bot.add_to_env(env)
+    GripperBot.add_to_env(env)
+    
+
+    #return (UR5Bot, UR5Bot.q, GanBot, GanBot.q)
+    return
 
 
 if __name__ == '__main__':
     env = swift.Swift()
-    env.launch(realtime = True)
-    GanBot = GantryBot()
+    transform = SE3(0,0,0)
+    #UR5Bot, UR5q, GanBot, Ganq = 
+    CreateEnvironment(env, transform)
+    
     #GanBot.teach(GanBot.q)
-    GanBot.test()
-    GanBot.add_to_env(env)
+    #GanBot.test()
+
     input('press enter to close')
+
+
--- robotcore/Gripper/Gripper.py
+++ robotcore/Gripper/Gripper.py
@@ -1,5 +1,6 @@
 import os
 from math import pi
+import swift
 
 import roboticstoolbox as rtb
 import spatialmath.base as spb
@@ -9,70 +10,50 @@ from ir_support.robots.DHRobot3D import DHRobot3D
 
 
 class Gripper(DHRobot3D):
-    """Gripper Class holds two finger classes and manages both"""
-
-    def __init__(self, env):
-        self.left_finger = GripperFinger()
-        self.right_finger = GripperFinger()
-        self.left_finger.add_to_env(env)
-        self.right_finger.add_to_env(env)
-
-        self.open = [-0.4 * pi, 0.4 * pi]
-        self.close = [-0.2 * pi, 0.2 * pi]
-        self.finger_offset = SE3(-50e-3, 0, 60e-3)  # Offset of the first finger joint
-
-        self.base_offset = SE3(-4e-3, -2e-3, 81e-3)
-        self.tool_offset = SE3(0, 0, 210e-3)
-
-        # DH links
-        links = [rtb.RevoluteDH(d=0, a=0, alpha=0, qlim=[0, 0])]  # Need to have one link, this should never move
-
-        # Names of the robot link files in the directory
-        link_names = dict(link0='Base', color0=(0.2, 0.2, 0.2, 1),
-                          link1='Base', color1=(0.2, 0.2, 0.2, 1))  # Duplicate
-
-        qtest = [0]
-        qtest_transforms = [spb.transl(0, 0, 0), spb.transl(0, 0, 0)]
-
-        current_path = os.path.abspath(os.path.dirname(__file__))
-        super().__init__(links, link_names, name='Gripper', link3d_dir=current_path, qtest=qtest,
-                         qtest_transforms=qtest_transforms)
-
-        self.q = qtest
-        self.q_last = self.open
-        self.add_to_env(env)
-
-    def setq(self, q):
-        """Update finger positions and rotation"""
-        if q is None:
-            q = self.q_last
-        self.left_finger.base = self.base * self.finger_offset * SE3.Ry(q[0]) * SE3.Rx(-pi / 2)
-        self.left_finger.q = 0  # q[1]  # Poke q values to fix bug
-
-        # Build gripper as radially symmetric
-        self.right_finger.base = self.base * SE3.Rz(pi) * self.finger_offset * SE3.Ry(q[0]) * SE3.Rx(-pi / 2)
-        self.right_finger.q = 0  # q[1]
-
-        self.q = self.q  # Fix issue described by Quang regarding overloading
-        self.q_last = q
-
-
-class GripperFinger(DHRobot3D):
-    """Gripper Finger"""
     def __init__(self):
         # DH links
-        links = [rtb.RevoluteDH(d=0, a=5, alpha=0, qlim=[-pi, pi])]
+        links = self._create_DH()
 
         # Names of the robot link files in the directory
-        link_names = dict(link0='Larm', color0=(0.1, 0.1, 0.1, 1),
-                          link1='Ltool', color1=(0.1, 0.1, 0.1, 1))
+        link_names = dict(link0='GripperBase', color0 = (0.2, 0.2, 0.2, 1),  # color option only takes effect for stl file
+                          link1='GripperPiece2', color1 = (0.2, 0.2, 0.2, 1))
 
         # A joint config and the 3D object transforms to match that config
         qtest = [0]
-        qtest_transforms = [spb.transl(50e-3, 0, 0) @ spb.trotx(pi / 2),
-                            spb.transl(30e-3, -35e-3, 0) @ spb.trotx(pi / 2)]
+        qtest_transforms = [spb.transl(0, 0.1, 0) @ spb.rpy2tr(-pi/2, 0, -pi/2, order = 'xyz'),
+                            spb.transl(0, 0.1, 0) @ spb.rpy2tr(-pi/2, 0, -pi/2, order = 'xyz')]
 
         current_path = os.path.abspath(os.path.dirname(__file__))
-        super().__init__(links, link_names, name='GripperHalf', link3d_dir=current_path, qtest=qtest,
+        super().__init__(links, link_names, name='GantryBot', link3d_dir=current_path, qtest=qtest,
                          qtest_transforms=qtest_transforms)
+        self.base = self.base * SE3.Rx(pi/2) * SE3.Ry(pi/2)
         self.q = qtest
+
+        self.q = [0]  # Known safe start values
+        self.links[0].qlim = [0, 0.03]
+
+    @staticmethod
+    def _create_DH():
+        """
+        Create robot's standard DH model
+        """
+        links = [rtb.PrismaticDH(theta=0, a=0, alpha=pi/2, qlim=[0, 0.03])]  # Prismatic Link
+        return links
+    
+    def test(self):
+        """
+        Test the class by adding 3d objects into a new Swift window and do a simple movement
+        """
+        env = swift.Swift()
+        env.launch(realtime= True)
+        self.q = self._qtest        
+        self.add_to_env(env)
+        q_goal = [0.03]
+        qtraj = rtb.jtraj(self.q, q_goal, 50).q
+        # fig = self.plot(self.q, limits= [-1,1,-1,1,-1,1])
+        # fig._add_teach_panel(self, self.q)
+        for q in qtraj:
+            self.q = q
+            env.step(0.02)
+            # fig.step(0.01)
+        # fig.hold()
