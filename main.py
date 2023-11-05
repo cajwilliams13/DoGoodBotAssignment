327b7784c85ea87ffcc055375d360dcc2330b576
DeltaQyto <deltaqyto@gmail.com>
2023-11-05 20:09:37 +1100

    Cleanup to environment setup

--- main.py
+++ main.py
@@ -26,14 +26,11 @@ def create_sim_env(env, master_transform=None):
     master_transform = SE3() if master_transform is None else master_transform
 
     table_offset = SE3(0, 0, 0.65)  # Height of table
-    gate_len = 1.215  # Length of gate
-
     props = [
         Prop('objects\\Table', env, transform=master_transform * table_offset * SE3(-1.5, 1.2, 0), color=(99, 71, 32)),
         Prop('objects\\extinguisher', env, transform=master_transform * SE3(-2, 1.35, 0), color=(102, 15, 13)),
         Prop('objects\\Storage', env, transform=master_transform * SE3(-0.5, -0.5, 0.65) * SE3.Rz(pi / 2),color=(80, 60, 15)),
         Prop('objects\\printer', env, transform=master_transform * table_offset * SE3(0.3, -0.95, 0.02) * SE3.Rz(pi),color=(0, 0, 1)),
-        #Prop('objects\\Holder', env, is_stl=False, transform=master_transform * table_offset * SE3(0.8, -0.95, 0) * SE3.Rz(pi)),
         Prop('objects\\Floor', env, transform=master_transform * table_offset * SE3(3, 2.5, -1) * SE3.Rz(pi/2), color = (100, 10, 10)),
         Prop('objects\\Walls', env, transform=master_transform * table_offset * SE3(3, 2.5, -1) * SE3.Rz(pi/2), color = (10, 10, 100)),
         Prop('objects\\OverheadLight', env, transform=master_transform * table_offset * SE3(0.2, -0.1, 1.5), color=(200, 200, 200)),
@@ -45,20 +42,19 @@ def create_sim_env(env, master_transform=None):
 
     ]
 
-    # Use XYZRz encoded position
+    estop_button2 = EStop(initial_pose=SE3(0, -1, 0.65))
+    estop_button2.add_to_env(env)
+
+
+    # Use XYZRz encoded position for gates
     gate_locations = [(-1.5, -2.4, 0, 0), (-1.5, 1.15, 0, 0)]
 
     extra_gate_locations = [(*g[:2], 0.6, g[3]) for g in gate_locations]  # Add a second layer of gates
     extra_gate_locations += [(*g[:2], 1.2, g[3]) for g in gate_locations]  # Add a third layer of gates
     gate_locations += extra_gate_locations
-    # props.append(Prop('objects\\Estop', env, color=(100, 0, 0), transform=master_transform * table_offset *
-    #                                                                      SE3(1.82 - gate_len, gate_len - 0.02, 0.561)))
 
     for gate in gate_locations:
         props.append(Prop('objects\\w2h4_fence', env, transform=master_transform, position=gate, color=(50, 50, 50)))
-
-    estop_button2 = EStop(initial_pose=SE3(0, -1, 0.65))
-    estop_button2.add_to_env(env)
 
     return props
 
