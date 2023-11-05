051cee85baaca8f7bf7e3b5b3ac17be5e58ad62f
DeltaQyto <deltaqyto@gmail.com>
2023-11-05 19:40:43 +1100

    Fixed bug in gripper2
    Base must be set before q is updated to trigger base update

--- Gripper2/Gripper2.py
+++ Gripper2/Gripper2.py
@@ -4,7 +4,6 @@ import swift
 
 import roboticstoolbox as rtb
 import spatialmath.base as spb
-from spatialmath import SE3
 
 from ir_support.robots.DHRobot3D import DHRobot3D
 
@@ -15,13 +14,13 @@ class Gripper2(DHRobot3D):
         links = self._create_DH()
 
         # Names of the robot link files in the directory
-        link_names = dict(link0='GripperBase', color0 = (0.2, 0.2, 0.2, 1),  # color option only takes effect for stl file
-                          link1='GripperPiece2', color1 = (0.2, 0.2, 0.2, 1))
+        link_names = dict(link0='GripperBase', color0=(0.2, 0.2, 0.2, 1),  # color option only takes effect for stl file
+                          link1='GripperPiece2', color1=(0.2, 0.2, 0.2, 1))
 
         # A joint config and the 3D object transforms to match that config
         qtest = [0]
-        qtest_transforms = [spb.transl(0, 0.1, 0) @ spb.rpy2tr(-pi/2, 0, -pi/2, order = 'xyz'),
-                            spb.transl(0, 0.1, 0) @ spb.rpy2tr(-pi/2, 0, -pi/2, order = 'xyz')]
+        qtest_transforms = [spb.transl(0, 0.1, 0) @ spb.rpy2tr(-pi / 2, 0, -pi / 2, order='xyz'),
+                            spb.transl(0, 0.1, 0) @ spb.rpy2tr(-pi / 2, 0, -pi / 2, order='xyz')]
 
         current_path = os.path.abspath(os.path.dirname(__file__))
         super().__init__(links, link_names, name='GantryBot', link3d_dir=current_path, qtest=qtest,
@@ -29,7 +28,7 @@ class Gripper2(DHRobot3D):
         self.q = qtest
 
         self.q = [0]  # Known safe start values
-        #self.links[0].qlim = [0, 0.03]
+        # self.links[0].qlim = [0, 0.03]
         self.open = [0.02]
         self.close = [0]
         self.qlast = self.q
@@ -38,24 +37,24 @@ class Gripper2(DHRobot3D):
         if q is None:
             q = self.qlast
         self.qlast = self.q
-        self.q = q
         self.base = self.base
+        self.q = q
 
     @staticmethod
     def _create_DH():
         """
         Create robot's standard DH model
         """
-        links = [rtb.PrismaticDH(theta=0, a=0, alpha=pi/2, qlim=[0, 0.03])]  # Prismatic Link
+        links = [rtb.PrismaticDH(theta=0, a=0, alpha=pi / 2, qlim=[0, 0.03])]  # Prismatic Link
         return links
-    
+
     def test(self):
         """
         Test the class by adding 3d objects into a new Swift window and do a simple movement
         """
         env = swift.Swift()
-        env.launch(realtime= True)
-        self.q = self._qtest        
+        env.launch(realtime=True)
+        self.q = self._qtest
         self.add_to_env(env)
         q_goal = [0.03]
         qtraj = rtb.jtraj(self.q, q_goal, 50).q
